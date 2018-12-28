6d5949554ecd914053e7aca88c51d6e8c518e981
mikeymanoguerra <mikeymanoguerra@gmail.com>
2018-12-28 10:37:15 -0500

    hardcode url failed, need to research CORS for that specific api

--- scripts/api.js
+++ scripts/api.js
@@ -5,6 +5,7 @@ const api = (function(){
 
   const ISS_URL = 'http://api.open-notify.org/iss-now.json?callback=?';
   const MAP_URL = 'https://open.mapquestapi.com/staticmap/v5/map?key=KEY&center=';
+  const mapTestURL = 'https://open.mapquestapi.com/staticmap/v5/map?key=KEY&center=-11.2631,-10.9335&size=@2x';
   
   const getISSdata = function(callback){
     $.getJSON(`${ISS_URL}`, callback);
@@ -24,7 +25,8 @@ const api = (function(){
     console.log('getMapImage ran');
     const coordinatesString = getISSdata(getCoordinates);
     console.log(coordinatesString);
-    $.getJSON(`${MAP_URL}${coordinatesString}&size=@2x`,callback);
+    // $.getJSON(`${MAP_URL}${coordinatesString}&size=@2x`,callback);
+    $.getJSON(`${mapTestURL}`, callback);
   }
 
 // use those coordinates to get picture of the map open static map api
