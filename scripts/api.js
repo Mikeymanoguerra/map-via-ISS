98f642cc80200f19091ec393f17ed2c9b9272c72
mikeymanoguerra <mikeymanoguerra@gmail.com>
2018-12-30 12:21:41 -0500

    resave

--- scripts/api.js
+++ scripts/api.js
@@ -6,50 +6,54 @@ const api = (function(){
   
   const ISS_URL = 'http://api.open-notify.org/iss-now.json?callback=?';
   const MAP_URL = 'https://www.mapquestapi.com/staticmap/v5/map?key=Zv1VRVyg4mhlWC8AmfdyyfhLhZS5EGcO&center=';
-  const NASA_URL ='https://api.nasa.gov/planetary/apod?api_key=6xjJbi1FBz5ogidAuBgD1CNzpVDhMlopTUXgFTLY'
-  const nasaDemo = 'https://api.nasa.gov/planetary/earth/imagery?lon=101.75&lat=7.5&dim=.5&cloud_score=true&api_key=6xjJbi1FBz5ogidAuBgD1CNzpVDhMlopTUXgFTLY';
+  const NASA_URL = 'https://api.nasa.gov/planetary/earth/imagery?';
+  const api_key = 'api_key=6xjJbi1FBz5ogidAuBgD1CNzpVDhMlopTUXgFTLY';
+  const testCoordinates = 'lon=-42.4009&lat=-13.9241';
 
-  const getISSdata = function(callback){
-    $.getJSON(`${ISS_URL}`, callback);
-  };
-
-  const getCoordinates = function(data){
+  const  getCoordinates = function(data){
     const latitude = data['iss_position'].latitude;
     const longitude =data['iss_position'].longitude;
-    const coorString =`${latitude},${longitude}`;
+    const coorString =`lon=${longitude}&lat=${latitude}`;
     console.log(coorString);
     return coorString;
   };
 
-  const coordinates = getISSdata(getCoordinates);
-  // need to add a promise somehow
+  const htmlToDom = function(data){ 
+    const url = data.url;
+    const htmlString = `<img src="${url}" alt="ehh">`;
+    $('.js-results').html(htmlString); 
+  };
 
+  const getISSdata = new Promise(function(){
+    $.getJSON(`${ISS_URL}`, getCoordinates);
+  });
 
+  // getISSdata.then(function(coorString){
+  //   console.log('NASA ajax ran');
+  //   $.getJSON(`${NASA_URL}${coorString}&cloud_score=false&dim=.5&${api_key}`, htmlToDom);
+  // });    
 
-  const getNasaImage = function(callback){
-    $.getJSON(`${nasaDemo}`, callback);
+  const getNasaImage = function (){
+    console.log('NASA ajax ran');
+    $.getJSON(`${NASA_URL}${testCoordinates}&cloud_score=true&dim=.3&${api_key}`, htmlToDom);
   };
 
 
- 
+
+  // use those coordinates to get picture of the map open static map api
+
+  // use those coordinates to get the data from nasa sattelite data
 
 
-
-// use those coordinates to get picture of the map open static map api
-
-// use those coordinates to get the data from nasa sattelite data
+  // use those coordinates to determine if over land or water on water API
 
 
-// use those coordinates to determine if over land or water on water API
-
-
-// use IP address of user to get dead reckoning or something
+  // use IP address of user to get dead reckoning or something
 
   return{
     getISSdata,
-    getCoordinates,
-    getNasaImage,
-    coordinates,
+    getNasaImage
+   
   };
 
 }());
--- scripts/index.js
+++ scripts/index.js
@@ -4,11 +4,6 @@
 
 $(document).ready(function() {
   console.log('end');
-  // api.getISSdata(api.getCoordinates);
-  api.getNasaImage( function(data){ 
-    const url = data.url;
-    const htmlString = `<img src="${url}" alt="ehh">`;
-    $('.js-results').html(htmlString);
-  });
-
+  api.getNasaImage();
 });
+
