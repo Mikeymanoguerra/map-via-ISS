ff2fdf6854247f38b58aa0f3299c895825781948
mikeymanoguerra <mikeymanoguerra@gmail.com>
2019-03-07 20:45:51 -0500

    build script ;\

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
+    publicPath: "/Views-via-ISS/", 
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js'
   },
